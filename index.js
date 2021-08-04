571d0656ae44e6120567cbb4a41aa0b4788262f4
Pooja28s <pooja.1997sehgal@gmail.com>
2021-08-04 12:56:07 +0530

    fix: changed html file to js

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>How well do you know me</title>
-</head>
-<body>
-    var readlineSync = require("readline-sync")
+
+var readlineSync = require("readline-sync")
 
 var list = [
   {
@@ -69,5 +61,3 @@ var highScore = [
 displayHighScore();
 
 
-</body>
-</html>
